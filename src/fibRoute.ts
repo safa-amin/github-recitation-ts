1e6cfd2232118ccd03126b09f4abe07567f34c33
Sarim Faraz <sarimfaraz@dyn-172-20-64-43.qatar.cmu.local>
2024-08-31 21:45:46 +0300

    fixed the any type value issue

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import fibonacci from 'src/fib.ts';
+import fibonacci from './fib';
 
 // Endpoint for querying the fibonacci numbers
 export default (req: Request, res: Response) => {
@@ -7,7 +7,7 @@ export default (req: Request, res: Response) => {
 
   const parsedNum: number = parseInt(num, 10);
   const fibN: number = fibonacci(parsedNum);
-  let result: string = `fibonacci(${num}) is ${fibN}`;
+  let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
