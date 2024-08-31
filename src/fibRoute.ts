e8539e791bc8a8fa6a538ba57adcb58de6f0d9b1
Safa Rida Riaz Amin <safaamin@dyn-172-20-103-13.qatar.cmu.local>
2024-08-31 21:20:39 +0300

    Refactor fibRoute.ts to improve type safety and error handling

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,8 +1,7 @@
-// Endpoint for querying the fibonacci numbers
-
+import { Request, Response } from 'express';
 const fibonacci = require("./fib");
 
-export default (req, res) => {
+export default (req: Request, res: Response) => {
   const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num));
